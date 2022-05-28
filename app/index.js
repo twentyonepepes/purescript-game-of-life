1d405a0232b573bf817d2711c3affe9676be4b12
daniel <daniel@savages.io>
2022-05-28 10:20:59 -0400

    PepeLaugh

--- app/index.js
+++ app/index.js
@@ -1,5 +1,27 @@
-const { field } = require('../output/Game');
+import delay from "delay";
+const { field, nextField } = require('../output/Game');
+const { toIndexedArray } = require('../output/Matrix');
 
-console.log("You're the app now, dog");
+(async function(){
 
-window.field = field
+	let a = field(64)(64);
+	let target = document.getElementById("Field");
+	target.innerHTML = toIndexedArray(a).map(({x,y,value})=>{
+		return (
+			`<div id=NX${x}Y${y} style="position:absolute; top:${y * 10}; left:${x * 10}; width: 10; height: 10; backgroundColor: ${value ? "white" : "blue"};"></div>`
+		)
+	}).join("");
+
+	while(true) {
+
+		a = nextField(a);
+		let b = toIndexedArray(a);
+		for (let {x,y,value} of b) {
+			let el = document.getElementById(`NX${x}Y${y}`);
+			el.style.backgroundColor = value ? "lightgray" : "steelblue";
+		}
+		await delay (100);
+	}
+
+
+})()
